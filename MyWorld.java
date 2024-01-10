c91e9640caabb52daa55c38b9d09fc7a16b636d6
JustYe <341153946@gapps.yrdsb.ca>
2024-01-10 13:00:07 -0500

    Adding a loop function which will be expanded on later as I make more worlds

--- MyWorld.java
+++ MyWorld.java
@@ -21,4 +21,34 @@ public class MyWorld extends World
         Bob bob = new Bob();
         addObject(bob, 500, 474);
     }
+    
+    public void checkTeleport()
+    {
+        Bob bob = (Bob) getObjects(Bob.class).get(image);
+
+        int worldWidth = getWidth();
+        int worldHeight = getHeight();
+
+        if (bob.getX() <= 0)
+        {
+            bob.setLocation(worldWidth - 1, bob.getY());
+        }
+        else if (bob.getX() >= worldWidth)
+        {
+            bob.setLocation(1, bob.getY());
+        }
+        
+        if (bob.getY() <= 0)
+        {
+            bob.setLocation(bob.getX(), worldHeight - 1);
+        }
+        else if (bob.getY() >= worldHeight)
+        {
+            bob.setLocation(bob.getX(), 1);
+        }
+    }
+    
+    public void act() {
+        checkTeleport();
+    }
 }
