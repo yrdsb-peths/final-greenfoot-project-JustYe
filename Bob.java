ca94ac28f8fc0f0307d4476902a7ce04786298e0
JustYe <341153946@gapps.yrdsb.ca>
2024-01-10 12:50:45 -0500

    Added sqaure

--- Bob.java
+++ Bob.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  
 
 /**
- * Write a description of class Bob here.
+ * Write a description of class Bob here."
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -12,33 +12,17 @@ public class Bob extends Actor
      * Act - do whatever the Bob wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    GreenfootImage[] idleRight = new GreenfootImage[7];
-    GreenfootImage[] idleLeft = new GreenfootImage[7];
-    String facing = "left";
-    SimpleTimer animationTimer = new SimpleTimer();
+    GreenfootImage image = new GreenfootImage("SQUARE.png");
     
     public Bob()
     {
-        for(int i = 0; i < idleRight.length; i++)
-        {
-            idleRight[i] = new GreenfootImage("images/TheHammer/hammercharge" + i + ".png");
-            idleRight[i].mirrorHorizontally();
-            idleRight[i].scale(300,300);
-        }
-        
-        for(int i = 0; i < idleLeft.length; i++)
-        {
-           idleLeft[i] = new GreenfootImage("images/TheHammer/hammercharge" + i + ".png");
-           idleLeft[i].scale(300,300); 
-        }
-        
-        animationTimer.mark();
+       image.scale(image.getWidth() / 4, image.getHeight()/4); 
+       setImage(image);
     }
     
     public void act()
     {
         checkKeys();
-        animateBob();
     }
     public void checkKeys()
     {
@@ -49,36 +33,6 @@ public class Bob extends Actor
         else if(Greenfoot.isKeyDown("d"))
         {
             move(5);
-        }
-    }
-    public void animateBob()
-    {
-        int imageIndex = 0;
-        if(animationTimer.millisElapsed() < 400)
-        {
-            return;
-        }
-        animationTimer.mark();
-        
-        if(facing.equals("left"))
-        {
-            if(!Greenfoot.isKeyDown("a"))
-            {
-                setImage(idleRight[0]);
-                return;
-            }
-            setImage(idleLeft[imageIndex]);
-            imageIndex = (imageIndex + 1) % idleLeft.length;
-        }
-        else if(facing.equals("right"))
-        {
-            if(!Greenfoot.isKeyDown("d"))
-            {
-                setImage(idleRight[0]);
-                return;
-            }
-            setImage(idleRight[imageIndex]);
-            imageIndex = (imageIndex + 1) % idleRight.length;
         }
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -16,9 +16,9 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1000,700, 1);
+        super(1000,500,1);
         
         Bob bob = new Bob();
-        addObject(bob, 500, 500);
+        addObject(bob, 500, 474);
     }
 }
