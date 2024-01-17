ce4783b7863ed0df3d1a9363034be2f9445374e6
JustYe <341153946@gapps.yrdsb.ca>
2024-01-16 23:08:42 -0500

    new titlescreen

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Shooting Sim",50);
+    Label titleLabel = new Label("Square dodging bullet",50);
     /**
      * Constructor for objects of class TitleScreen.
      * 
